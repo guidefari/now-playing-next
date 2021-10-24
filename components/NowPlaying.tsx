d0859cefdae55dde5eb0cbb4acda670b0d27814b
Guide Fari <guideg6@gmail.com>
2021-10-25 00:09:47 +0200

    some styles

--- components/NowPlaying.tsx
+++ components/NowPlaying.tsx
@@ -24,13 +24,21 @@ const NowPlaying: React.FC<NowPlayingProps> = ({
                 title={`Album cover for ${title} by ${artist}. From the album ${album}`}
               />
             </a>
-            <figcaption className="text-2xl text-center dark:text-yellow-600">
+            <figcaption className="text-2xl font-black text-center dark:text-yellow-600">
               {album}
             </figcaption>
           </figure>
-          <h1 className="my-24 text-6xl font-black tracking-tight text-center dark:text-yellow-500">
-            {artist} - {title}
-          </h1>
+          <aside>
+            <a
+              href={songUrl}
+              target="_blank"
+              className="hover:underline hover:text-yellow-600"
+            >
+              <h1 className="my-24 text-6xl font-black tracking-tight text-center dark:text-yellow-500">
+                {artist} - {title}
+              </h1>
+            </a>
+          </aside>
         </section>
       )}
     </>
