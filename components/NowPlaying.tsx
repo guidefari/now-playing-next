344e3021b89451314a3a57d257c8233153b8d933
Guide Fari <guideg6@gmail.com>
2021-10-24 23:43:10 +0200

    fixed the render issue caused by undefined

--- components/NowPlaying.tsx
+++ components/NowPlaying.tsx
@@ -1,34 +1,28 @@
 import React from 'react'
 import useSWR from 'swr'
 import fetcher from '../lib/fetcher'
+import { NowPlayingProps } from '../lib/types'
 
-type NowPlayingProps = {
-  album?: string
-  albumImageUrl?: string
-  artist?: string
-  isPlaying: boolean
-  songUrl?: string
-  title?: string
-}
-
-const NowPlaying: React.FC = ({}) => {
-  const { data } = useSWR('/api/now-playing', fetcher)
-
-  const { album, albumImageUrl, artist, isPlaying, songUrl, title } =
-    data as NowPlayingProps
-
+const NowPlaying: React.FC<NowPlayingProps> = ({
+  album,
+  isPlaying,
+  albumImageUrl,
+  title,
+  artist,
+}) => {
   return (
     <>
       {isPlaying && (
-        <section>
-          <h1 className="my-24 text-6xl font-black tracking-tight text-center dark:text-yellow-500">
-            Now Playing {title}
-          </h1>
+        <section className="flex ">
           <img
             src={albumImageUrl}
+            className="rounded-xl"
             alt={`Album cover for ${title} by ${artist}. From the album ${album}`}
             title={`Album cover for ${title} by ${artist}. From the album ${album}`}
           />
+          <h1 className="my-24 text-6xl font-black tracking-tight text-center dark:text-yellow-500">
+            Now Playing {title}
+          </h1>
         </section>
       )}
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,25 @@
 import NowPlaying from '../components/NowPlaying'
+import useSWR from 'swr'
+import fetcher from '../lib/fetcher'
+import { NowPlayingProps } from '../lib/types'
 
 function Homepage(): JSX.Element {
-  return <NowPlaying />
+  const { data } = useSWR<NowPlayingProps>('/api/now-playing', fetcher)
+
+  if (!data) {
+    return <div></div>
+  }
+
+  return (
+    <NowPlaying
+      isPlaying={data.isPlaying}
+      album={data.album}
+      albumImageUrl={data.albumImageUrl}
+      artist={data.artist}
+      songUrl={data.songUrl}
+      title={data.title}
+    />
+  )
 }
 
 export default Homepage
